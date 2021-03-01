8366514e33bb7d7c7f96d3033ab8a58ef509329f
Artyom Peredery <artperedery@gmail.com>
2021-03-01 18:09:51 +0300

    remove yarn per project install

---
+++ __test__/binarySeach.test.js
@@ -0,0 +1,9 @@
+import binarySearch from "../01/binarySearch";
+
+test('Binary Search', () => {
+  const list1 = [0, 1, 2, 3, 4, 5, 6, 7];
+  const emptyList = [];
+  expect(binarySearch(emptyList, 1)).toBe(null);
+  expect(binarySearch(list1, 4)).toBe(4);
+  expect(binarySearch(list1, 6)).toBe(6);
+})
